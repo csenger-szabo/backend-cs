574a63386deeda6e7316273b231b4e292cfaab53
Csenger Szabo <csengerszabo@gmail.com>
2019-07-22 21:13:15 +0200

    express configured

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,15 @@
+import express = require('express');
+const app: express.Application = express();
+
 export const startApp = async () => {
   // create application
+  app.get('/', (_req, res) => {
+    res.send('hello my friend');
+  });
+
   // start application
+  app.listen(3000, () => {
+    console.log('App is listening on localhost:3000');
+  })
+
 };
